b2dc4a0c88f95bf4b2f2ca4980f76e8acceab00d
GunjanAr <gunjan.arora@emerson.com>
2024-08-04 18:14:36 +0530

    episode 2

--- App.js
+++ App.js
@@ -1,25 +1,7 @@
-{/* <div id="parent">
-	<div id="child">
-	        <h1> I am h1 tag</h1>
-            <h2> I am h2 tag</h2>
-	</div>
-    <div id="child2">
-	        <h1> I am h1 tag</h1>
-            <h2> I am h2 tag</h2>
-	</div>
-</div> */}
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 
-const parent = React.createElement("div", { id: "parent" }, [
-    React.createElement("div", { id: "child" }, [
-        React.createElement("h1", {}, "I am h1 tag"),
-        React.createElement("h2", {}, "I am h2 tag")
-    ]),
-    React.createElement("div", { id: "child2" }, [
-        React.createElement("h1", {}, "I am h1 tag"),
-        React.createElement("h2", {}, "I am h2 tag")
-    ])
-    ]
-)
-console.log(parent)
-const Root = ReactDOM.createRoot(document.getElementById("root"))
-Root.render(parent);
+const heading = React.createElement("h1", { id: "heading" }, "Hello World! from Gunjan");
+console.log(heading)
+const Root = ReactDOM.createRoot(document.getElementById("root"));
+Root.render(heading);
--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # Namaste React Practice by Gunjan Arora.
+
+# Parcel
+
+- Dev Build
+- Local Server
+- HMR = Hot Module Replacement.
+- File Watching Algorithm - written in C++
+- Caching - Faster Builds
+- Image Optimization
+- Minification of our file while creating production build
+- Bundling while creating production build
+- Compress the files while creating production build
+- Consistent Hashing
+- Code Splitting
+- Differentail Bundling - support older Browsers
+- Diagnostic
+- Error Handling
+- HTTPs
+- Tree Shaking - remove unused code
+- Different dev and prod bundles
